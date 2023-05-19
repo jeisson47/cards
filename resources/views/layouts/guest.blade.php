b367ee383f360c63e4195895e16652defffd6005
jeisson47 <jeissont62@hotmail.com>
2023-05-19 18:33:39 +0200

    add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,20 +19,20 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="{{asset('storage/assets/img/favicon.png')}}">
+  <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
   <title>
      | @yield('title')
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="{{asset('storage/assets/css/nucleo-icons.css')}}" rel="stylesheet" />
-  <link href="{{asset('storage/assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/349ee9c857.js" crossorigin="anonymous"></script>
-  <link href="{{asset('storage/assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="{{asset('storage/assets/css/corporate-ui-dashboard.css')}}?v=1.0.0" rel="stylesheet" />
+  <link id="pagestyle" href="{{asset('assets/css/corporate-ui-dashboard.css')}}?v=1.0.0" rel="stylesheet" />
 </head>
 
 <body class="">
@@ -105,10 +105,10 @@
       @yield('content')
 
   <!--   Core JS Files   -->
-  <script src="{{asset('storage/assets/js/core/popper.min.js')}} "></script>
-  <script src="{{asset('storage/assets/js/core/bootstrap.min.js')}}"></script>
-  <script src="{{asset('storage/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
-  <script src="{{asset('storage/assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
+  <script src="{{asset('assets/js/core/popper.min.js')}} "></script>
+  <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+  <script src="{{asset('assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
+  <script src="{{asset('assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -121,7 +121,7 @@
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{asset('storage/assets/js/corporate-ui-dashboard.min.js')}}?v=1.0.0"></script>
+  <script src="{{asset('assets/js/corporate-ui-dashboard.min.js')}}?v=1.0.0"></script>
 </body>
 
 </html>
