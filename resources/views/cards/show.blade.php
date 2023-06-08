b715488ea4f591d58a93ba362db5ea4c26d64cb3
jeisson47 <jeissont62@hotmail.com>
2023-06-07 21:16:47 -0500

    Update show.blade.php

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('assets/css/cards/global.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>{{$card->name_person}}</title>
 
     <style>
@@ -19,8 +20,23 @@
             background-color: var(--bs-background) !important;
             border-radius: 0px;
         }
-        .bg-primary{
-            background-color: var(--bs-primary) !important;
+        .btn-outline-primary{
+            color: #000;
+            background-color: #fff;
+            border:2px solid var(--bs-primary) !important;
+            transition: 1s ease all;
+        }
+        .btn-outline-primary i{
+            visibility: hidden;;
+            transition: 1s all ease;
+        }
+        .btn-outline-primary:hover{
+            transition: 1s ease all;
+            background-color: #fff;
+            color: #000;
+        }
+        .btn-outline-primary:hover i{
+            visibility: visible;
         }
     </style>
 
@@ -68,7 +84,7 @@
         </div>
         <div class="modal-footer border-0">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <a href="{{$card->calendar}}" class="btn bg-primary" target="_blanck">Entiendo, crear cita</a>
+          <a href="{{$card->calendar}}" class="btn btn-outline-primary" target="_blanck">Entiendo, crear cita <i class="bi bi-arrow-bar-right"></i></a>
         </div>
       </div>
     </div>
